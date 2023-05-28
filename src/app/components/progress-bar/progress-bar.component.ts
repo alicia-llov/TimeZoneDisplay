3091f2934866d9c5746b9ee8220c0b73ead9b3f8
Alicia Llovera <a.llovera@ride-on.es>
2023-05-28 11:59:04 +0200

    Create first approach to progress bar

---
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input, OnInit, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
+import * as moment from 'moment';
+import { LocalStorageService } from '../../services/localStorage/local-storage.service'
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.scss']
+})
+export class ProgressBarComponent {
+
+  @Input()
+  selectedTimeZone: string = ''
+
+  dotPercentage: string = '';
+  dotValue: number;
+  currentTimeDisplay: string;
+
+  constructor(private localStorageService: LocalStorageService) {}
+
+  ngOnInit(): void {
+    
+    this.calculatePercentage()
+    setInterval(() => {
+        this.calculatePercentage()
+        console.log('')
+    }, 60000);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.timeByTimeZone(changes['selectedTimeZone'].currentValue)
+  }
+
+  calculatePercentage() {
+    const secondsInADay = 24 * 60 * 60;
+    const now = moment().tz(this.selectedTimeZone)
+    const hours = now.hours() * 60 * 60;
+    const minutes = now.minutes() * 60;
+    const seconds = now.seconds();
+    const totalSeconds = hours + minutes + seconds;
+
+    this.dotValue = Math.round(100 * totalSeconds/secondsInADay)
+    this.dotPercentage = 100 * totalSeconds/secondsInADay + '%';
+  }
+
+  timeByTimeZone(timeZone: string) {
+    this.localStorageService.setItem('selectedTimeZone', timeZone)
+    this.currentTimeDisplay = moment().tz(timeZone).format("HH:mm:ss")
+    this.calculatePercentage()
+  }
+
+}
