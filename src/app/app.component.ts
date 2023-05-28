8053caba949427997e0a7e93bf8044bc9723e08e
Alicia Llovera <a.llovera@ride-on.es>
2023-05-28 12:01:26 +0200

    Compose view in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { LocalStorageService } from './services/localStorage/local-storage.service'
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'infovista';
+export class AppComponent implements OnInit{
+
+  selectedTimeZone: string;
+  
+  constructor(private localStorageService: LocalStorageService) {}
+
+  ngOnInit(): void {
+    this.selectedTimeZone = this.localStorageService.getItem('selectedTimeZone') || moment.tz.guess(true);
+  }
+
+  getTimeZone(timeZone: string) {
+    this.selectedTimeZone = timeZone
+  }
 }
